7352b77ad198b1b52df776425ca3e8c3f353128a
Marek Zelinka <mzelinka17@gmail.com>
2024-10-17 20:16:30 +0200

    setup redis

--- src/routes/restaurants.ts
+++ src/routes/restaurants.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 import { validate } from "../utils/middleware.js";
+import { initRedisClient } from "../utils/redis.js";
 
 export const restaurantsRouter = express
   .Router()
@@ -9,5 +10,6 @@ export const restaurantsRouter = express
   })
   .post("/", validate(RestaurantSchema), async (req, res) => {
     const data = req.body as Restaurant;
+    await initRedisClient();
     res.send("Hello world");
   });
---
+++ src/utils/redis.ts
@@ -0,0 +1,17 @@
+import { createClient, type RedisClientType } from "redis";
+import { logError, logInfo } from "./logger.js";
+
+let redis: RedisClientType | null = null;
+
+export async function initRedisClient() {
+  if (!redis) {
+    redis = createClient();
+
+    redis.on("error", (err) => logError("Redis Client Error", err));
+    redis.on("ready", () => logInfo("Redis Client Started"));
+
+    await redis.connect();
+  }
+
+  return redis;
+}
