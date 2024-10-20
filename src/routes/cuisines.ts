319f88bfd5816606cf615f67b34a9b26a98fa370
Marek Zelinka <mzelinka17@gmail.com>
2024-10-20 16:19:11 +0200

    add cuisines routes

--- src/routes/cuisines.ts
+++ src/routes/cuisines.ts
@@ -1,3 +1,37 @@
 import express from "express";
+import {
+  getCuisineKeyByName,
+  getCuisinesKey,
+  getRestaurantKeyById,
+} from "../utils/keys.js";
+import { getCurrentRedisClient } from "../utils/redis.js";
+import { successResponse } from "../utils/responses.js";
 
-export const cuisinesRouter = express.Router();
+export const cuisinesRouter = express
+  .Router()
+  .get("/", async (_req, res) => {
+    const redisClient = await getCurrentRedisClient();
+
+    const cuisinesKey = getCuisinesKey();
+
+    const cuisines = await redisClient.sMembers(cuisinesKey);
+
+    successResponse({ res, data: { cuisines } });
+  })
+  .get("/:cuisine", async (req, res) => {
+    const redisClient = await getCurrentRedisClient();
+
+    const cuisine = req.params.cuisine;
+    const cuisinesKey = getCuisineKeyByName(cuisine);
+
+    const restaurantIds = await redisClient.sMembers(cuisinesKey);
+    const restaurants = await Promise.all(
+      restaurantIds.map((id) => {
+        const restaurantKey = getRestaurantKeyById(id);
+
+        return redisClient.hGet(restaurantKey, "id");
+      }),
+    );
+
+    successResponse({ res, data: { restaurants } });
+  });
